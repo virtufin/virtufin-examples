64116826454d1a4524bfb5fbe6503642e9f41b53
Patrick Haener <contact@haenerconsulting.com>
2014-04-08 12:14:59 +0200

    Fixed discount curve

--- src/main/scala/examples/finance/simulation/MonteCarloPricingExample.scala
+++ src/main/scala/examples/finance/simulation/MonteCarloPricingExample.scala
@@ -10,16 +10,7 @@ import virtufin.finance.product.Currency._
 import virtufin.finance.product.feature._
 import virtufin.finance.simulation._
 import virtufin.finance.simulation.EuropeanPlainVanillaOption._
-import java.util.Calendar._
-import akka.actor.{Props, ActorSystem}
-import akka.pattern._
-import scala.concurrent.duration._
-import scala.collection.SortedSet
-
 import virtufin.finance.Time
-import akka.util.Timeout
-import play.api.libs.iteratee.{Enumeratee, Iteratee, Enumerator}
-import scala.concurrent.ExecutionContext.Implicits.global
 import virtufin.simulation.{Message, AgentIdentifier}
 import virtufin.finance.scenario.Price
 import virtufin.finance.model.BlackParametersIdentifier
@@ -29,14 +20,21 @@ import virtufin.finance.product.Stock
 import virtufin.finance.product.Position
 import virtufin.finance.product.PortfolioNode
 import virtufin.finance.product.PositionNode
-import scala.concurrent.Future
-
 // implicit for converting to Simulatable
 import virtufin.finance.simulation.PortfolioHierarchyAgent._
 import virtufin.finance.model.ScenarioRequest
 import virtufin.finance.product.EuropeanPlainVanillaOption
 // implicit for Strategy and for converting to Simulatable
 import virtufin.finance.product.EuropeanPlainVanillaOption._
+import java.util.Calendar._
+import akka.actor.{Props, ActorSystem}
+import akka.pattern._
+import akka.util.Timeout
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.collection.SortedSet
+import scala.concurrent.ExecutionContext.Implicits.global
+import play.api.libs.iteratee.{Enumeratee, Iteratee, Enumerator}
 
 /**
  * Example for Monte Carlo valuation on a portfolio level
@@ -82,10 +80,10 @@ object MonteCarloPricingExample extends Example {
     )
     )
   )
-  implicit val portfolioHierarchyAgent:AgentIdentifier = "portfolioHierarchy"
+  implicit val portfolioHierarchyAgent: AgentIdentifier = "portfolioHierarchy"
 
   // Observables and dates we need for model
-  val trades = Seq((id0:PortfolioIdentifier) -> trade0, (id1:PortfolioIdentifier) -> trade1)
+  val trades = Seq((id0: PortfolioIdentifier) -> trade0, (id1: PortfolioIdentifier) -> trade1)
   val observables = trades.map(t => t._2.marketObservable)
   val fixings = trades.flatMap(t => t._2.fixings)
   val fixingDates = trades.flatMap(t => t._2.fixingDates)
@@ -93,10 +91,10 @@ object MonteCarloPricingExample extends Example {
   val eventDates = SortedSet[Time](fixingDates ++ settlementDates: _*).toArray
 
   // We will put these into the Agents object
-  val simulatables:Iterable[(AgentIdentifier,Simulatable)]=List(
-    (id0:AgentIdentifier)->(trade0:Simulatable),
-    (id1:AgentIdentifier)->(trade1:Simulatable),
-    portfolioHierarchyAgent->(portfolioHierarchy:Simulatable)
+  val simulatables: Iterable[(AgentIdentifier, Simulatable)] = List(
+    (id0: AgentIdentifier) -> (trade0: Simulatable),
+    (id1: AgentIdentifier) -> (trade1: Simulatable),
+    portfolioHierarchyAgent -> (portfolioHierarchy: Simulatable)
   )
 
   // The scenario at t0
@@ -107,6 +105,7 @@ object MonteCarloPricingExample extends Example {
 
 
   // Some implicits for default values
+
   import RandomGenerator.defaultRandomGenerator
   import UncorrelatedStandardNormalRandomNumberGenerator.defaultUncorrelatedStandardRandomNumberGenerator
   import MultivariateWienerIncrements.defaultMultivariateWienerIncrements
@@ -137,68 +136,65 @@ object MonteCarloPricingExample extends Example {
   implicit val modelDispatcher = ModelDispatcher()
   val context = MultivariateBlackScholesScenarioModel.modelContext(observables, blackParameters, eventDates)
   val numberScenarios = 10
+
   def scenarios = model.model(request, context).get.map(s => Enumerator.enumerate(s)).take(numberScenarios)
+
   def scenarioEnumerator = Enumerator.enumerate(scenarios)
 
   implicit val timeout = Timeout(10 seconds)
   val system = ActorSystem("MonteCarloPricingExample")
 
-  def createSimulation(system:ActorSystem, scenario:Enumerator[QuotesEvent], simulatables:Iterable[(AgentIdentifier,Simulatable)]):virtufin.finance.simulation.Simulation={
+  def createSimulation(system: ActorSystem, scenario: Enumerator[QuotesEvent], simulatables: Iterable[(AgentIdentifier, Simulatable)]): virtufin.finance.simulation.Simulation = {
     val agents = system.actorOf(Props(Agents))
     for {
-      f <- Future.traverse(simulatables)(t=>agents ? AddAgent(t._1, t._2))
+      f <- Future.traverse(simulatables)(t => agents ? AddAgent(t._1, t._2))
     } yield f
     virtufin.finance.simulation.Simulation(scenario, agents)
   }
 
   // Create an Enumerator of Simulation objects
   def simulations = scenarioEnumerator.map(s => createSimulation(system, s, simulatables))
+
   // An Enumerator of an Enumerator of MessagesEvents
   def transactions = simulations.map(s => s.generateTransactions())
+
   // Transform that into an Enumerator of Lists of MessagesEvents
   val collectTransactions = Iteratee.fold(List[MessagesEvent]())((l: List[MessagesEvent], x: MessagesEvent) => l.::(x))
+
   // Create a Cashflows object if the Message is a Transfer of Cash and if either the source
   // or target Portfolio are as given in the argument. I.e. we filter out all the cashfrom from/to
   // a specific Porfolio
-  def messageToCashflow(t:Day, sourceOrTargetPortfolio:PortfolioIdentifier):PartialFunction[Message,Pair[Day,Pair[Currency,Double]]]={
-    case Message(m:Transfer[_],id) if m.position.asset.isInstanceOf[Currency] &&
-      (m.sourcePortfolio == sourceOrTargetPortfolio||m.targetPortfolio==sourceOrTargetPortfolio)=>
-        if(m.sourcePortfolio==sourceOrTargetPortfolio) t-> (m.position.asset.asInstanceOf[Currency],m.position.amount)
-        else t-> (m.position.asset.asInstanceOf[Currency],-1.0*m.position.amount)
-  }
-  def messagesEventsToCashflows(messagesEvents:List[MessagesEvent],sourceOrTargetPortfolio:PortfolioIdentifier):Map[Currency,Cashflows]={
-     val x=messagesEvents.flatMap(e=>e.messages.map(m=>messageToCashflow(e.time,sourceOrTargetPortfolio)(m)))
-     x.groupBy(a=>a._2._1).map(b=>(b._1,b._2.map(c=>c._1->c._2._2))).map(d=>(d._1,Cashflows(d._1,d._2)))
+  def messageToCashflow(t: Day, sourceOrTargetPortfolio: PortfolioIdentifier): PartialFunction[Message, Pair[Day, Pair[Currency, Double]]] = {
+    case Message(m: Transfer[_], id) if m.position.asset.isInstanceOf[Currency] &&
+      (m.sourcePortfolio == sourceOrTargetPortfolio || m.targetPortfolio == sourceOrTargetPortfolio) =>
+      if (m.sourcePortfolio == sourceOrTargetPortfolio) t ->(m.position.asset.asInstanceOf[Currency], m.position.amount)
+      else t ->(m.position.asset.asInstanceOf[Currency], -1.0 * m.position.amount)
   }
 
-  val sourceOrTargetPortfolio:PortfolioIdentifier=sellerPortfolio0
+  def messagesEventsToCashflows(messagesEvents: List[MessagesEvent], sourceOrTargetPortfolio: PortfolioIdentifier): Map[Currency, Cashflows] = {
+    val x = messagesEvents.flatMap(e => e.messages.map(m => messageToCashflow(e.time, sourceOrTargetPortfolio)(m)))
+    x.groupBy(a => a._2._1).map(b => (b._1, b._2.map(c => c._1 -> c._2._2))).map(d => (d._1, Cashflows(d._1, d._2)))
+  }
+
+  val sourceOrTargetPortfolio: PortfolioIdentifier = sellerPortfolio0
+
   // Apply now the transformations:
-  def transactionsLists=transactions.map(t => t |>>> collectTransactions) &> Enumeratee.mapM(identity)
+  def transactionsLists = transactions.map(t => t |>>> collectTransactions) &> Enumeratee.mapM(identity)
+
   // This gives an Enumerator mapping currencies to Cashflows objects
-  def cashflows = transactionsLists.map(a=>messagesEventsToCashflows(a,sourceOrTargetPortfolio))
+  def cashflows = transactionsLists.map(a => messagesEventsToCashflows(a, sourceOrTargetPortfolio))
 
   // Value the cashflows
-  def value(cashflows:Map[Currency,Cashflows], modelDate:Day, discountCurves:Map[Currency,Double=>Double]):Map[Currency,Cash]={
-     cashflows.map(c=>c._1->CashflowsPricingModel.price(c._2,modelDate,discountCurves(c._1)))
+  def value(cashflows: Map[Currency, Cashflows], modelDate: Day, discountCurves: Map[Currency, Double => Double]): Map[Currency, Cash] = {
+    cashflows.map(c => c._1 -> CashflowsPricingModel.price(c._2, modelDate, discountCurves(c._1)))
   }
-  val r=0.02
-  val discountCurves=Map[Currency,Double=>Double](USD->(t=>r))
-  def values = cashflows map(c=>value(c, t0, discountCurves))
-  // Output the results
-  object outputScenarios
-  object outputTransactionsLists
-  object outputCashflows
-  object outputValues
-  val outputSelector=outputValues
-  def outputResult(s:Any) {
-    s match {
-      case outputScenarios => output(outputScenarios)
-      case outputTransactionsLists => transactionsLists |>>> Iteratee.foreach(output)
-      case outputCashflows => cashflows |>>> Iteratee.foreach(output)
-      case outputValues => println(">>>"); values |>>> Iteratee.foreach(output)
-    }
-  }
-  output("Transactions")
+
+  val r = 0.02
+  val discountCurves = Map[Currency, Double => Double](USD -> (t => Math.exp(-r*t)))
+
+  def values = cashflows map (c => value(c, t0, discountCurves))
+
+  // Output
   transactionsLists |>>> Iteratee.foreach(output)
   Thread.sleep(2000)
   output("\nCashflows")
