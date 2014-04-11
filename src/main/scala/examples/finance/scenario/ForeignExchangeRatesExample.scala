4d2502e887cd7c4913423d7d860456cfaa440f40
Patrick Haener <contact@haenerconsulting.com>
2014-04-11 13:22:07 +0200

    Changed name from Foreign ExchangeRates to ForeignExchangeQuotes

--- src/main/scala/examples/finance/scenario/ForeignExchangeRatesExample.scala
+++ src/main/scala/examples/finance/scenario/ForeignExchangeRatesExample.scala
@@ -2,14 +2,14 @@ package examples.finance.scenario
 
 import virtufin.finance.product.Currency._
 
-import virtufin.finance.scenario.{ForeignExchangeRate, ForeignExchangeRates}
+import virtufin.finance.scenario.{ForeignExchangeRate, ForeignExchangeRateQuotes}
 import examples.Example
 
 object ForeignExchangeRatesExample extends Example {
   val referenceCurrency = USD
   // price quoted referenceCurrency of a unit of some currency
   val data = List(EUR -> 1.375, GBP -> 1.664, AUD -> 0.9252)
-  val quotes = ForeignExchangeRates(referenceCurrency, data: _*)
+  val quotes = ForeignExchangeRateQuotes(referenceCurrency, data: _*)
   // get cross
   val cross = ForeignExchangeRate(AUD, EUR)
   val value = quotes.get(cross)
